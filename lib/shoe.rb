10f8c32dea6aba32661b173cb681a21e94bffef1
github username <github email address>
2019-11-01 00:28:26 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,17 @@
 # Make your shoe class here!
+class Shoe
+
+    attr_reader :new
+    attr_accessor :size, :color, :condition, :material, :brand
+    
+    def initialize(new_shoes)
+        @new_shoes = new_shoes
+        @brand = "Nike"
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+
+end
